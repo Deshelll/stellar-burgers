3a8cd5b30e4a4fd4d3c28d4606d6a3b92f402e99
Andrey Koblov <a.coblov@yandex.ru>
2025-04-22 01:24:14 +0400

    Fix protected route

--- src/components/Protected-Route/index.tsx
+++ src/components/Protected-Route/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector } from '../../services/store';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 import { RootState } from 'src/services/store';
 
 interface ProtectedRouteProps {
@@ -13,13 +13,15 @@ export const ProtectedRoute = ({
   unAuth = false
 }: ProtectedRouteProps) => {
   const user = useSelector((state: RootState) => state.auth.user);
+  const location = useLocation();
+  const from = location.state?.from || '/';
 
   if (unAuth && user) {
-    return <Navigate to='/' />;
+    return <Navigate to={ from } />;
   }
 
   if (!user && !unAuth) {
-    return <Navigate to='/login' replace />;
+    return <Navigate to='/login' state={{ from: location }} />;
   }
 
   return children;
